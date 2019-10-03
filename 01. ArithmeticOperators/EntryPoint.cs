05022ea96030195bf6bcaf71071638cc1da73adc
Prescott Lawani <prescott.lawani@gmail.com>
2019-10-03 14:15:21 -0500

    This should add the new AssignmentOperators project

--- 01. ArithmeticOperators/EntryPoint.cs
+++ 01. ArithmeticOperators/EntryPoint.cs
@@ -8,6 +8,9 @@ class EntryPoint
         int firstNumber = 36;
         int secondNumber = 33;
         int thirdNumber = 777;
+
+        int fourthNumber = 3; int fifthNumber = 7; int sixthNumber = 5; int seventhNumber = 4;
+
         
         int addition = firstNumber + secondNumber;
         int subtraction = secondNumber - firstNumber;
@@ -23,7 +26,32 @@ class EntryPoint
         double equation = (double)firstNumber * ((double)secondNumber - (double)thirdNumber);
         int multiplication2 = secondNumber * thirdNumber;
         Console.Write("" + firstNumber + " times the difference of " + thirdNumber + " and " + secondNumber + " = " + equation + "\n");
-        Console.Write("" + secondNumber + " * " + thirdNumber);
+        Console.Write("" + secondNumber + " * " + thirdNumber + "\n");
+
+        //equation2 and equation3 are the same operation
+        double equation2 = (double)(((fourthNumber/fifthNumber)) + Math.Pow(sixthNumber, 2)) / (double)((fourthNumber) + (seventhNumber/fourthNumber));
+        Console.Write("" + fourthNumber + "/" + fifthNumber + " + " + sixthNumber + " squared " +
+            " / " + fourthNumber + " + " + seventhNumber + "/" + fourthNumber + " = " + equation2 + "\n");
+
+        double equation3 = ((3 / 7) + Math.Pow(5, 2)) / (3 + (4 / 3));
+        Console.Write(equation3 + "\n");
+
+        // ++, and -- increase and decrease the value of an interger by 1, based on the location
+        Console.WriteLine(++firstNumber);
+        Console.WriteLine(firstNumber--);
+        Console.WriteLine(firstNumber);
+
+        //Modulous, the remained of division is used with the % symbol and only shows what is left of a division operation
+        int remainder = thirdNumber % firstNumber;
+        Console.WriteLine(remainder);
+
+        //Using 2 with modulous gives a 1 or a 0 to show if the remainer is odd or even
+        int remainder2 = 4 % 2;
+        Console.WriteLine(remainder2);
+
+
+
+        
     }
 }
 
---
+++ 02 AssignmentOperators/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class EntryPoint
+{
+    static void Main()
+    {
+        //Assignment operators consist of =, +=, and -=
+        int firstNumber = 5; int secondNumber = 6;
+        firstNumber += secondNumber; //This means firstNumber = firstNumber + secondNumber
+        Console.WriteLine(firstNumber); //Instead of 5, this will equal 11
+
+        secondNumber -= firstNumber; //This means the secondNumber = secondNumber - the new firstNumber (11), which is now -5
+        //but if we use the increment operator to display it, we will get instead:
+        Console.WriteLine(++secondNumber);//-4
+
+        //There is also *=, /=. and %= operators/
+
+
+
+    }
+}
+
