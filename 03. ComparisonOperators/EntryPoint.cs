25fc137e0ce4e1c2f501154d44fef0359b484b1b
Prescott Lawani <prescott.lawani@gmail.com>
2019-10-03 16:34:50 -0500

    I'm still trying to figure out why every commit and sync causes the github to have the same comment on all the files, projects, and the solution

---
+++ 03. ComparisonOperators/EntryPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+    class EntryPoint
+    {
+        static void Main()
+        {
+        //There are 6 ComparisonOperators
+        // > Greater than; >= Greater than or equal to
+        // < Less than; <= Less than or equal to
+        // == Equal to; != Not equal to
+
+        int firstNumber = 18;
+        int secondNumber = 36;
+
+        Console.WriteLine(firstNumber > secondNumber); //boolean result of true or false
+        Console.WriteLine(secondNumber < firstNumber);
+
+        int thirdNumber = 33;
+        int fourthNumber = 33;
+        Console.WriteLine(thirdNumber >= fourthNumber); //true
+        Console.WriteLine(thirdNumber != fourthNumber); //false
+
+        //Since the ComparisonOperators are true or false, you can set them to boolean datatypes
+        bool isThirdNumberEqualToFourthNumber = fourthNumber == thirdNumber;
+        Console.WriteLine(isThirdNumberEqualToFourthNumber); //true
+
+        int fifthNumber = 36;
+        int sixthNumber = 33;
+        bool isFifthNumberLessThanOrEqualToSixthNumber = fifthNumber <= sixthNumber;
+        Console.WriteLine(fifthNumber <= sixthNumber);//false
+        Console.WriteLine(isFifthNumberLessThanOrEqualToSixthNumber);//false
+
+        //Strings can be compared as well. You use the length property to do this.
+        string nameOne = "tod";
+        
+        bool nameOneVersusnameTwo = (nameOne.Length > "Tod".Length);
+        Console.WriteLine(nameOneVersusnameTwo);
+        }
+    }
---
+++ 04. LogicalOperators/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class EntryPoint
+{
+    static void Main()
+    {
+        // && - AND Operator Only if both
+        // || - OR Operator If either
+        // ! - Not Operator The opposite of the other
+
+        int number = 5;
+        bool lessThan = number < 20;
+        bool moreThan = number > 15;
+
+        bool isInRange = lessThan && moreThan;
+
+        Console.WriteLine(lessThan);
+        Console.WriteLine(moreThan);
+        Console.WriteLine(isInRange);
+
+
+    }
+}
+
